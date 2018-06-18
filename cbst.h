1494fc697033fe7ce67e7072fd0924650011d103
LucaLumetti <lumetti.luca@gmail.com>
2018-06-18 11:19:40 +0200

    free

--- cbst.h
+++ cbst.h
@@ -39,8 +39,8 @@ tree_##NAME bst_##NAME##_insert(TYPE e, tree_##NAME t){                       \
 tree_##NAME bst_##NAME##_delete(TYPE e, tree_##NAME t) {                      \
   tree_##NAME l = t, pl = NULL, pr = NULL, next = NULL;                       \
   while(!tree_##NAME##_isEmpty(t) &&                                          \
-         (CMPFN)(&e, &(t->value)) != 0) {                                      \
-    if((CMPFN)(&e, &(t->value)) < 0) {                                         \
+        (CMPFN)(&e, &(t->value)) != 0) {                                      \
+    if((CMPFN)(&e, &(t->value)) < 0) {                                        \
       pl = t;                                                                 \
       pr = tree_##NAME##_new();                                               \
       t = tree_##NAME##_left(t);                                              \
@@ -57,25 +57,39 @@ tree_##NAME bst_##NAME##_delete(TYPE e, tree_##NAME t) {                      \
                                                                               \
   if (tree_##NAME##_isEmpty(tree_##NAME##_left(t)) &&                         \
       tree_##NAME##_isEmpty(tree_##NAME##_right(t))) {                        \
-    if (t == l)                                                               \
+    if (t == l) {                                                             \
+      free(t);                                                                \
       return tree_##NAME##_new();                                             \
-    if (!tree_##NAME##_isEmpty(pl))                                           \
+    }                                                                         \
+    if (!tree_##NAME##_isEmpty(pl)){                                          \
+      free(pl->left);                                                         \
       pl->left = tree_##NAME##_new();                                         \
-    else                                                                      \
+    } else {                                                                  \
+      free(pr->right);                                                        \
       pr->right = tree_##NAME##_new();                                        \
+    }                                                                         \
   }                                                                           \
                                                                               \
   else if (tree_##NAME##_isEmpty(tree_##NAME##_left(t)) &&                    \
            !tree_##NAME##_isEmpty(tree_##NAME##_right(t))) {                  \
-    if (t == l)                                                               \
-      return tree_##NAME##_right(t);                                          \
+    if (t == l) {                                                             \
+      pl = tree_##NAME##_right(t);                                            \
+      free(t);                                                                \
+      return pl;                                                              \
+    }                                                                         \
     next = tree_##NAME##_right(t);                                            \
+    free(t);                                                                  \
   }                                                                           \
+                                                                              \
   else if (!tree_##NAME##_isEmpty(tree_##NAME##_left(t)) &&                   \
     tree_##NAME##_isEmpty(tree_##NAME##_right(t))) {                          \
-    if (t == l)                                                               \
-      return tree_##NAME##_left(t);                                           \
+    if (t == l) {                                                             \
+      pl = tree_##NAME##_left(t);                                             \
+      free(t);                                                                \
+      return pl;                                                              \
+    }                                                                         \
     next = tree_##NAME##_left(t);                                             \
+    free(t);                                                                  \
   }                                                                           \
                                                                               \
   else if (!tree_##NAME##_isEmpty(tree_##NAME##_left(t)) &&                   \
@@ -89,15 +103,21 @@ tree_##NAME bst_##NAME##_delete(TYPE e, tree_##NAME t) {                      \
       next = tree_##NAME##_left(next);                                        \
     }                                                                         \
     t->value = tree_##NAME##_root(next);                                      \
-    if (!tree_##NAME##_isEmpty(tree_##NAME##_right(next)))                    \
-      next = tree_##NAME##_right(next);                                       \
-    else                                                                      \
+    if (!tree_##NAME##_isEmpty(tree_##NAME##_right(next))) {                  \
+      tree_##NAME tmp = tree_##NAME##_right(next);                            \
+      free(next);                                                             \
+      next = tmp;                                                             \
+    }                                                                         \
+    else {                                                                    \
+      free(next);                                                             \
       next = tree_##NAME##_new();                                             \
+    }                                                                         \
   }                                                                           \
-  if (!tree_##NAME##_isEmpty(pl))                                             \
+  if (!tree_##NAME##_isEmpty(pl)) {                                           \
     pl->left = next;                                                          \
-  else                                                                        \
+  } else {                                                                    \
     pr->right = next;                                                         \
+  }                                                                           \
   return l;                                                                   \
 }
 #endif /* BST_H */
